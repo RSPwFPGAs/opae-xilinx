6c0092a69239399b36273e1ade7220b49d33aeb3
riple <riple@riple.com>
2019-09-30 13:15:28 +0800

    Added AFU stand-alone project files.

---
+++ hw/prj/afu/create_proj.tcl
@@ -0,0 +1,3 @@
+create_project proj_opae_afu ./proj_opae_afu -part xcku040-ffva1156-2-e -f
+set_property board_part xilinx.com:kcu105:part0:1.5 [current_project]
+
---
+++ hw/prj/afu/edt_proj.tcl
@@ -0,0 +1,5 @@
+close_project -quiet
+source create_proj.tcl
+
+source ../../src/ipi/afu_setup_flow.tcl
+

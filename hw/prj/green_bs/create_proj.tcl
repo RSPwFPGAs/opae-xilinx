a85fc62830bcc4a4352fbb5145a9cc4d0d9fb175
riple <riple@github.com>
2019-11-21 06:39:52 +0800

    Added makefile variable AFU_IP_PATH for manually customizing AFUs.

--- hw/prj/green_bs/create_proj.tcl
+++ hw/prj/green_bs/create_proj.tcl
@@ -1,3 +1,6 @@
 create_project proj_opae_gbs ./proj_opae_gbs -part xcku040-ffva1156-2-e -f
 set_property board_part xilinx.com:kcu105:part0:1.5 [current_project]
 
+set_property  ip_repo_paths [lindex $argv 1] [current_project]
+update_ip_catalog
+
---
+++ hw/src/afu_customize/hls_ip/adder_axilite/add_hls_ip.tcl
@@ -0,0 +1,4 @@
+
+set_property  ip_repo_paths  ./hls_prj/solution1/impl/ip [current_project]
+update_ip_catalog
+

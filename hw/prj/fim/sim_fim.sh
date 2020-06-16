addcfea3fbeb8395909687026127e48b01c4f403
RSPwFPGAs <RSPwFPGAs@github.com>
2020-06-16 14:42:48 +0800

    Updated simulation script for FIM with ethernet interface.

--- hw/prj/fim/sim_fim.sh
+++ hw/prj/fim/sim_fim.sh
@@ -10,6 +10,8 @@ elif [ $fim_path == ../../src/ipi/fim_default.bd.tcl ]; then
 	sed -i 's/xvlog_opts="--relax/xvlog_opts="--relax -d PCIE_INST=pcie3_ultrascale_0/g' board.sh
 elif [ $fim_path == ../../src/ipi/fim_debug_u50dd.bd.tcl ]; then
 	sed -i 's/xvlog_opts="--relax/xvlog_opts="--relax -d PCIE_INST=xdma_0 -d U50DD=1/g' board.sh
+else
+	sed -i 's/xvlog_opts="--relax/xvlog_opts="--relax -d PCIE_INST=axi_pcie3_0.inst.pcie3_ip_i/g' board.sh
 fi
 sed -i 's/xsim board/xsim -gui -view ..\/..\/..\/src\/sim\/board_behav.wcfg -onerror stop -onfinish stop board/g' board.sh
 
